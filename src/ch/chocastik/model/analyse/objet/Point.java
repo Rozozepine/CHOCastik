6ba41a9bab3d0a462a472188c38e2aaba5b76248
Rose <rose.defossez@gmail.com>
2019-04-23 11:00:49 +0200

    erreur branche

--- src/ch/chocastik/model/analyse/objet/Point.java
+++ src/ch/chocastik/model/analyse/objet/Point.java
@@ -1,12 +1,49 @@
 package ch.chocastik.model.analyse.objet;
 
+import org.bytedeco.javacv.Frame;
+
 public class Point {
 	private int x;
 	private int y;
-	private final long startTime;
-	public Point(int x, int y) {
+	private Frame frame;
+	private int timecode;
+	
+	public Point(int x, int y, Frame frame, int timecode) {
 		this.x = x;
 		this.y = y;
-		this.startTime = System.currentTimeMillis();
+		this.frame = frame;
+		this.timecode = timecode;
+	}
+
+	public Frame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(Frame frame) {
+		this.frame = frame;
+	}
+
+	public int getTimecode() {
+		return timecode;
+	}
+
+	public void setTimecode(int timecode) {
+		this.timecode = timecode;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
 	}
 }
