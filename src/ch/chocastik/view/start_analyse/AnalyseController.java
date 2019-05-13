aac8f4045279f89007eda92f7c5d7b9782287e6b
alexia <alexia.lienhard@etu.unige.ch>
2019-05-13 11:45:16 +0200

    Analyse et accueil au complet :)

---
+++ src/ch/chocastik/view/start_analyse/AnalyseController.java
@@ -0,0 +1,29 @@
+package ch.chocastik.view.start_analyse;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Box;
+
+public class AnalyseController {
+
+    @FXML
+    private JFXButton Start;
+
+    @FXML
+    private JFXListView<?> ListGlisseur;
+
+    @FXML
+    private Box RetourCam;
+
+    @FXML
+    private ImageView Retourpoint;
+
+    @FXML
+    private JFXButton Reglage;
+
+    @FXML
+    private JFXButton Help;
+
+}
