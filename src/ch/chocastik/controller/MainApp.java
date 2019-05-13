69ae13b75706bcf9cdded019eff0f31c6f5352dc
Rose <rose.defossez@gmail.com>
2019-05-13 15:39:26 +0200

    Modif

--- src/ch/chocastik/controller/MainApp.java
+++ src/ch/chocastik/controller/MainApp.java
@@ -230,9 +230,6 @@ public class MainApp extends Application {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		 Loader.load(opencv_objdetect.class);
-		Point point = new Point(1, 2, 10);
-		Point point2 = new Point(1, 3, 10);
-		System.out.print(point.equals(point2));
 		launch(args);
 	}
 
